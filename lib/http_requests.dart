30fcc3013db536aef11180dd42e96c8c61b7b6d1
shervinmo <abtin@riseup.net>
2020-03-22 05:33:22 +0430

    add request handler

---
+++ lib/http_requests.dart
@@ -0,0 +1,38 @@
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import './moduls/lisence_moduls.dart';
+
+
+
+Future<License> fetchLicenseList() async {
+  final response =
+      await http.get('https://api.newbtin.ir/license/');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return License.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Licences');
+  }
+}
+
+
+
+Future<LicenseDetail> fetchLicense(licenseId) async {
+  final response =
+      await http.get('https://api.newbtin.ir/license/$licenseId');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return LicenseDetail.fromJson(json.decode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Licences');
+  }
+}
